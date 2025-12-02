5cccdd15e37825b4aa8121c795031f2ce8f73e50
mithridatem <mathieumithridate@adrar-formation.com>
2025-12-02 14:29:15 +0100

    feat : ConsoleRepository -> saveConsole + isExistsByName

--- src/com/adrar/games/Main.java
+++ src/com/adrar/games/Main.java
@@ -1,7 +1,22 @@
 package com.adrar.games;
 
+import com.adrar.games.model.Console;
+import com.adrar.games.repository.ConsoleRepository;
+
 public class Main {
     public static void main(String[] args) {
-
+        //Instance d'un objet console
+        Console playstation4 = new Console("Playstation 3", "Sony");
+        //instance d'un objet ConsoleRepository
+        ConsoleRepository consoleRepository = new ConsoleRepository();
+        //Test si la console existe
+        if (consoleRepository.isExistsByName(playstation4)) {
+            System.out.println("La console existe déja");
+        }
+        //Sinon elle n'existe pas
+        else {
+            //Appel de la méthode
+            System.out.println("La console à été ajouté, id : " + consoleRepository.saveConsole(playstation4).getId());
+        }
     }
 }
--- src/com/adrar/games/repository/ConsoleRepository.java
+++ src/com/adrar/games/repository/ConsoleRepository.java
@@ -3,10 +3,7 @@ package com.adrar.games.repository;
 import com.adrar.games.database.Request;
 import com.adrar.games.model.Console;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ConsoleRepository {
@@ -21,27 +18,71 @@ public class ConsoleRepository {
     }
 
     //Méthode pour ajouter une console (Console) en BDD
-    public Console saveConsole(Console console) throws SQLException
+    public Console saveConsole(Console console)
     {
-        //requête SQL
-        String sql = "INSERT INTO console (name, manufacturer) VALUES (?, ?)";
-        //Préparation de la requête
-        PreparedStatement pstmt = connect.prepareStatement(sql);
-        //Assigner les paramètres
-        pstmt.setString(1, console.getName());
-        pstmt.setString(2, console.getManufacturer());
-        //Exécuter la requête
-        int addRows = pstmt.executeUpdate();
-        return new Console();
-    }
+        try {
+            //requête SQL
+            String sql = "INSERT INTO console (name, manufacturer) VALUES (?, ?)";
+            //Préparation de la requête
+            PreparedStatement pstmt = connect.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            //Assigner les paramètres
+            pstmt.setString(1, console.getName());
+            pstmt.setString(2, console.getManufacturer());
+            //Exécuter la requête
+            int addRows = pstmt.executeUpdate();
 
+            //Test si la requête à fonctionné
+            if (addRows > 0) {
+                //récupération de l'ID
+                ResultSet rs = pstmt.getGeneratedKeys();
+                //Test si on à une réponse
+                if (rs.next()){
+                    //ide de l'enregistrement
+                    int id  = rs.getInt(1);
+                    //set de l' id
+                    console.setId(id);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return console;
+    }
+    //Méthode qui retourne une ArrayList de console (Console)
     public ArrayList<Console> findAll()
     {
         return new ArrayList<Console>();
     }
 
+    //Méthode qui retourne une console depuis son id
     public Console find(int id)
     {
         return new Console();
+    }
+
+    //méthode qui test si une console (Console) avec son name existe
+    public boolean isExistsByName(Console console)
+    {
+        try {
+            //Requête SQL
+            String sql = "SELECT c.id FROM console AS c WHERE c.`name` = ?";
+            //Préparer la requête
+            PreparedStatement pstmt = connect.prepareStatement(sql);
+            //Assigner le paramètre
+            pstmt.setString(1, console.getName());
+            //Exécution de la requête
+            ResultSet rs = pstmt.executeQuery();
+            //parcours du resultat
+            if (rs.next()) {
+                //test si l'id existe
+                if (rs.getInt("id") > 0) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
     }
 }
